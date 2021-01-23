bbd94710b99ce0309937d6fab2e866c6911917cb
Collins Nwoko <collinsnwoko05@gmail.com>
2021-01-24 00:41:34 +0100

    contact, footer and JS
    worked on the contact page, footer and scroll reveal. responsiveness for large screen added

--- dist/js/app.js
+++ dist/js/app.js
@@ -42,3 +42,36 @@ function scrollActive() {
     }
   });
 }
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+/*===== HOME =====*/
+sr.reveal(".home__title", {});
+sr.reveal(".home__scroll", { delay: 200 });
+sr.reveal(".home__img", { origin: "right", delay: 400 });
+/*===== ABOUT =====*/
+sr.reveal(".about__title", { delay: 500 });
+sr.reveal(".about__subtitle", { delay: 300 });
+sr.reveal(".about__profesion", { delay: 400 });
+sr.reveal(".about__text", { delay: 500 });
+sr.reveal(".about__social-icon", { delay: 400, interval: 200 });
+
+/*===== SKILLS =====*/
+sr.reveal(".skills__subtitle", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills__img ", { delay: 400 });
+
+/*===== PORTFOLIO =====*/
+sr.reveal(".portfolio__img", { interval: 200 });
+
+/*===== CONTACT =====*/
+sr.reveal(".contact__subtitle", {});
+sr.reveal(".contact__input", { delay: 400 });
+sr.reveal(".contact__text", { interval: 200 });
+sr.reveal(".contact__button", { delay: 600 });
